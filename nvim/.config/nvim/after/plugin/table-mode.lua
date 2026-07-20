14e86ad7d87b01208044e801536673c49189a20a
mtholomier <maxence.tholomier@kbrwadventure.com>
2026-07-20 17:14:22 +0200

    Make table mode activated by default in md files

--- nvim/.config/nvim/after/plugin/table-mode.lua
+++ nvim/.config/nvim/after/plugin/table-mode.lua
@@ -4,3 +4,10 @@ vim.g.table_mode_disable_tableize_mappings = 1
 vim.g.table_mode_map_prefix = "<LocalLeader>t"
 
 vim.keymap.set("n", "yot", ":TableModeToggle<CR>", { noremap = true, desc = "Toggle table mode" })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "markdown",
+	callback = function()
+		vim.cmd("TableModeEnable")
+	end,
+})
