ea3811aa7a0ec900a1683ffc9cd3b527a8e157c5
mtholomier <maxence.tholomier@kbrwadventure.com>
2026-07-21 17:02:46 +0200

    Remove useless aliases

--- bash/.bashrc.d/aliases.bash
+++ bash/.bashrc.d/aliases.bash
@@ -6,10 +6,4 @@ else
 	alias grep='grep --color=auto -i'
 fi
 
-cdd() {
-	dir=$(dirname "$1")
-	cd "$dir" || return
-}
-
 alias claude="claude --ide --chrome"
-alias claude-work='CLAUDE_CONFIG_DIR="$HOME/.claude-work" claude --ide --chrome'
