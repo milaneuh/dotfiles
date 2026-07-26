015fedf0d75d9d4edebf1ec10c4fe56f71ae3a8a
mtholomier <maxence.tholomier@kbrwadventure.com>
2026-07-26 15:00:33 +0200

    Update lua_ls config

--- nvim/.config/nvim/lsp/lua_ls.lua
+++ nvim/.config/nvim/lsp/lua_ls.lua
@@ -1,13 +1,40 @@
+local function has_own_luarc(client)
+
+  local path = client.workspace_folders[1].name
+  local config = vim.fn.stdpath("config")
+
+  if not client.workspace_folders then
+    return false
+  end
+
+  if vim.uv.fs_realpath(path) == vim.uv.fs_realpath(config) then
+    return false
+  end
+
+  if vim.uv.fs_stat(path .. "/.luarc.json") then
+    return true
+  end
+
+  if vim.uv.fs_stat(path .. "/.luarc.jsonc") then
+    return true
+  end
+
+  return false
+end
+
 return {
   on_init = function(client)
-    local path = client.workspace_folders[1].name
-    if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+    if has_own_luarc(client) then
       return
     end
 
     client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
       runtime = {
         version = "LuaJIT",
+        path = {
+          "lua/?.lua",
+          "lua/?/init.lua",
+        },
       },
       workspace = {
         checkThirdParty = false,
