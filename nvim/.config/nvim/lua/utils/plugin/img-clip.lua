d54961933c2631ac2f24002c94857d69ca77ec6d
mtholomier <maxence.tholomier@kbrwadventure.com>
2026-07-31 09:29:49 +0200

    Make img-clip rely on ENV variables

--- nvim/.config/nvim/lua/utils/plugin/img-clip.lua
+++ nvim/.config/nvim/lua/utils/plugin/img-clip.lua
@@ -1,10 +1,23 @@
 local M = {}
 
+local function require_dir(name)
+	local dir = os.getenv(name)
+
+	if not dir then
+		error("unset environment variable: " .. name)
+	end
+
+	if vim.fn.isdirectory(dir) == 0 then
+		error("missing zettelkasten directory: " .. name .. "=" .. dir)
+	end
+
+	return dir
+end
+
 function M.get_zettelkasten_info()
 	local buf_path = vim.fn.expand("%:p")
-	local home = os.getenv("HOME")
-	local zettelkasten_dir = home .. "/remoterepos/zettelkasten/notes"
-	local zettelkasten_company_dir = home .. "/travail/zettelkasten_company"
+	local zettelkasten_dir = require_dir("zettelkasten")
+	local zettelkasten_company_dir = require_dir("zettelkasten_company")
 
 	local is_zettelkasten = string.find(buf_path, zettelkasten_dir, 1, true) or
 		string.find(buf_path, zettelkasten_company_dir, 1, true)
