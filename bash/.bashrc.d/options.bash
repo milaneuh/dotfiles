b47880dd8866cc1b301d89125a59d4c608f371e9
mtholomier <maxence.tholomier@kbrwadventure.com>
2026-08-01 11:57:41 +0200

    One call to stty instead of two
    Improve performance

--- bash/.bashrc.d/options.bash
+++ bash/.bashrc.d/options.bash
@@ -1,7 +1,6 @@
 PS1='${RANGER_LEVEL:+(ranger) }${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 
-stty -ixon       # Disable terminal freezing with Ctrl+S
-stty lnext undef # Disable terminal quoting with Ctrl+V
+stty -ixon lnext undef # Disable Ctrl+S freezing and Ctrl+V quoting
 
 # set -o vi
 
