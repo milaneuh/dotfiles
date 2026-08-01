633319d35d13ae047e896892c7dd91ec09b66929
mtholomier <maxence.tholomier@kbrwadventure.com>
2026-08-01 12:34:42 +0200

    Do not check git history with vim.pack.get
    Only check if the plugin is present
    
    Improve performance

--- nvim/.config/nvim/lua/utils/pack.lua
+++ nvim/.config/nvim/lua/utils/pack.lua
@@ -3,7 +3,7 @@ local M = {}
 --- @param name string Plugin name (last segment of its source URL)
 --- @return boolean
 function M.loaded(name)
-  local ok, res = pcall(vim.pack.get, { name })
+  local ok, res = pcall(vim.pack.get, { name }, { info = false })
   return ok and #res > 0 and res[1].active
 end
 
