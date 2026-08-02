232ad6cfdaa763c13730b772bc47e6f660e03fed
mtholomier <maxence.tholomier@kbrwadventure.com>
2026-08-02 20:46:43 +0200

    Avoid hidden files in nvim and ranger

--- nvim/.config/nvim/after/plugin/oil.lua
+++ nvim/.config/nvim/after/plugin/oil.lua
@@ -5,7 +5,7 @@ require("oil").setup({
 	delete_to_trash = true,
 	skip_confirm_for_simple_edits = true,
 	view_options = {
-		show_hidden = true,
+		show_hidden = false,
 	},
 	sort = {
 		{ "type", "asc" },
