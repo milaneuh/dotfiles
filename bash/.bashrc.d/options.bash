bab25e960fc3ccb9b21fd9e525f32a6202ff90b4
mtholomier <maxence.tholomier@kbrwadventure.com>
2026-08-08 12:52:35 +0200

    Change bash prompt
    Were too long

--- bash/.bashrc.d/options.bash
+++ bash/.bashrc.d/options.bash
@@ -1,5 +1,4 @@
-# shellcheck disable=SC2154 # debian_chroot is set by /etc/bash.bashrc
-PS1='${RANGER_LEVEL:+(ranger) }${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+PS1='${RANGER_LEVEL:+(ranger) }\[\033[01;32m\]\u${SSH_CONNECTION:+@\h}\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 
 stty -ixon lnext undef # Disable Ctrl+S freezing and Ctrl+V quoting
 
