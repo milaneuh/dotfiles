49c004fa1554120d99f3ed3368772e41dff5776c
maxence <maxencetholomier@gmail.com>
2026-09-06 19:21:13 +0200

    Render mermaid through the sandboxed system Chrome

--- software/home/puppeteer/.bashrc.d/tools/puppeteer.bash
+++ software/home/puppeteer/.bashrc.d/tools/puppeteer.bash
@@ -1,11 +1,11 @@
 export MERMAID_FILTER_PUPPETEER_CONFIG="${XDG_CONFIG_HOME:-$HOME/.config}/puppeteer-config.json"
 
 for _p in \
-	"${HOME}/.nix-profile/bin/chromium" \
+	"/usr/bin/google-chrome" \
 	"/snap/chromium/current/usr/lib/chromium-browser/chrome" \
 	"/usr/bin/chromium" \
 	"/usr/bin/chromium-browser" \
-	"/usr/bin/google-chrome"; do
+	"${HOME}/.nix-profile/bin/chromium"; do
 	if [[ -x "$_p" ]]; then
 		export PUPPETEER_EXECUTABLE_PATH="$_p"
 		break
