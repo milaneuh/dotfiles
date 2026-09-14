fcf5c9edaef037c741cae7cc8e0bc6c2b60bb171
milaneuh <milanrgmnt@gmail.com>
2026-09-14 22:06:45 +0200

    Remove unused tool configurations

--- home/dot_bashrc.d/tools/asdf.bash
+++
@@ -1,9 +0,0 @@
-if [[ -d ~/.asdf ]]; then
-	export KERL_CONFIGURE_OPTIONS="--without-javac --without-wx --without-odbc --disable-jit"
-	export CFLAGS="-O2 -g -Wno-error=implicit-function-declaration -std=gnu11"
-	export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
-fi
-
-if command -v asdf &>/dev/null; then
-	cache_completion asdf
-fi
--- home/dot_bashrc.d/tools/opencode.bash
+++
@@ -1,2 +0,0 @@
-# To disable when markdown code block will be fixed https://github.com/anomalyco/opencode/issues/16470
-export OPENCODE_EXPERIMENTAL_MARKDOWN=0
